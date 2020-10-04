ef50317da93b0de4d361549f74d4695e20472bf9
Shakana Fox <foxshakana@gmail.com>
2020-10-04 18:36:27 -0400

    updated localstorage for times

--- script.js
+++ script.js
@@ -15,14 +15,14 @@ console.groupCollapsed(now);
 
 
 
- <!--1. HTML, CSS, Javascript documents -->
- <!-- 2. Scale out with boilerplate -->
- <!-- 3. Put moment.js link in to access jQuery library -->
- <!-- 4. Insert jumbotron from Bootstrap -->
- <!-- 5. Create div container for form  -->
- <!-- 6. Create an array for days of the week--> JQUERY
- <!-- 7. Create array for 9-5 workday hours--> JQUERY
- <!-- 8. create local storage to save text event -->
+//  <!--1. HTML, CSS, Javascript documents -->
+//  <!-- 2. Scale out with boilerplate -->
+//  <!-- 3. Put moment.js link in to access jQuery library -->
+//  <!-- 4. Insert jumbotron from Bootstrap -->
+//  <!-- 5. Create div container for form  -->
+//  <!-- 6. Create an array for days of the week--> JQUERY
+//  <!-- 7. Create array for 9-5 workday hours--> JQUERY
+//  <!-- 8. create local storage to save text event -->
 //  1. date and time
 
 //  3. display current date (location: top of calendar (CSS))
@@ -55,6 +55,16 @@ moment().format("MMM Do");
       localStorage.setItem("5PM", ($TEXT5PM.val()));
      });
 // 11. grab the users input - saveto local storage (onclick of step 10)
+ $("#text9AM").append(localStorage.getItem("9AM"));
+ $("#text10AM").append(localStorage.getItem("10AM"));
+ $("#text11AM").append(localStorage.getItem("11AM"));
+ $("#text12PM").append(localStorage.getItem("12PM"));
+ $("#text1PM").append(localStorage.getItem("1PM"));
+ $("#text2PM").append(localStorage.getItem("2PM"));
+ $("#text3PM").append(localStorage.getItem("3PM"));
+ $("#text4PM").append(localStorage.getItem("4PM"));
+ $("#text5PM").append(localStorage.getItem("5PM"));
+ 
 // 12. onload()- eventpreventdefault
 // 13. allow for the input to remain on the page - even on refresh
 
