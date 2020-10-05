c8129239d31a06ceb332b00499102b64d51c2375
Shakana Fox <foxshakana@gmail.com>
2020-10-05 13:23:26 -0400

    updated time variables

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 $(document).ready(function (){
-    console.log("this is loading");
+    //console.log("this is loading");
     //DOM VARIABLES
      var currentDay = $("#currentDay");
      var hour = $(".hour");
      var saveBtn = $(".saveBtn");
      var textareaDiv = $("textarea");
+
+     var text9AM = $("#text9AM");
+     var text10AM = $("#text10AM");
+     var text11AM = $("#text11AM");
+     var text12PM = $("#text12PM");
+     var text1PM = $("#text1PM");
+     var text2PM = $("#text2PM");
+     var text3PM = $("#text3PM");
+     var text4PM = $("#text4PM");
+     var text5PM = $("#text5PM");
    
     // JS VARIABLES
 
-    var currentTime =  moment().format("HH");
-    //var currentTimeInt = parseInt("currentTime");
-   var now = parseInt(moment().format('HH'));
-console.groupCollapsed(now);
+    var currentTime =  parseInt(moment().format('HM'));
+    var currentTimeInt = parseInt("currentTime");
+alert(currentTime);
 
+//var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 
 
 //  <!--1. HTML, CSS, Javascript documents -->
@@ -26,8 +36,8 @@ console.groupCollapsed(now);
 //  1. date and time
 
 //  3. display current date (location: top of calendar (CSS))
-moment().format('dddd'); 
-moment().format("MMM Do"); 
+// moment().format('dddd'); 
+// moment().format("MMM Do"); 
 
 //FUNCTION CALLS
 //  4. onscroll function (possible)
@@ -44,18 +54,18 @@ moment().format("MMM Do");
 // 10. eventlistener() - create a save button for each block to save the input (step 9)
 //     hint: jquery
      saveBtn.on("click", function (){
-      localStorage.setItem("9AM", ($TEXT9AM.VAL()));
-      localStorage.setItem("10AM", ($TEXT10AM.VAL()));
-      localStorage.setItem("11AM", ($TEXT11AM.val()));
-      localStorage.setItem("12PM", ($TEXT12PM.val()));
-      localStorage.setItem("1PM", ($TEXT1PM.val()));
-      localStorage.setItem("2PM", ($TEXT2PM.val()));
-      localStorage.setItem("3PM", ($TEXT3PM.val()));
-      localStorage.setItem("4PM", ($TEXT4PM.val()));
-      localStorage.setItem("5PM", ($TEXT5PM.val()));
+      localStorage.setItem("9AM", ($text9AM.val()));
+      localStorage.setItem("10AM", ($text10AM.val()));
+      localStorage.setItem("11AM", ($text11AM.val()));
+      localStorage.setItem("12PM", ($text12PM.val()));
+      localStorage.setItem("1PM", ($text1PM.val()));
+      localStorage.setItem("2PM", ($text2PM.val()));
+      localStorage.setItem("3PM", ($text3PM.val()));
+      localStorage.setItem("4PM", ($text4PM.val()));
+      localStorage.setItem("5PM", ($text5PM.val()));
      });
 // 11. grab the users input - saveto local storage (onclick of step 10)
- $("#text9AM").append(localStorage.getItem("9AM"));
+ $("#text9AM").append(localStorage.getItem("9AM")); //should I put .getItem before appending?
  $("#text10AM").append(localStorage.getItem("10AM"));
  $("#text11AM").append(localStorage.getItem("11AM"));
  $("#text12PM").append(localStorage.getItem("12PM"));
